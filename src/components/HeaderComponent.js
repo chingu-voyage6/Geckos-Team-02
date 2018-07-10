431a76f014cb9963b94a0d53d2aa70097c64ff52
Desmond <desmond.bera@gmail.com>
2018-07-09 23:44:02 -0400

    rough draft of modal login is set up

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,23 +7,42 @@ import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class HeaderComponent extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <header className="headerBox">
         <Navbar>
+
           <Navbar.Header>
             <Navbar.Brand>
               <a href="#brand">COB</a>
             </Navbar.Brand>
           </Navbar.Header>
+
           <Nav>
             <NavItem eventKey={1}>
               <ModalSignUpComponent />
             </NavItem>
-            <NavItem eventKey={2}>
-              <ModalLoginComponent />
+
+            <NavItem onClick={this.toggleModal} eventKey={2}>
+              Login
+              <ModalLoginComponent show={this.state.isOpen}/>
             </NavItem>
+
           </Nav>
+
         </Navbar>
       </header>
     );
--- src/components/ModalLoginComponent.js
+++ src/components/ModalLoginComponent.js
@@ -1,24 +1,65 @@
 import React from 'react';
 import '../css/App.css';
+import PropTypes from 'prop-types';
 
-import { Button, Modal } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 
 class ModalLoginComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false
-    };
-  }
+
   render() {
-    return (
-      <div>
-        <Button>
-          Login
-        </Button>
+    if(!this.props.show) {
+      return null;
+    }
+
+    const backDrop = {
+      position: 'fixed',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'rgba(0,0,0,0.3)',
+      padding: 50
+    };
+
+    const modalStyle = {
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      maxWidth: 500,
+      minHeight: 300,
+      margin: '0 auto',
+      padding: 30
+    };
+     return (
+      // <div className="backdropClass" style={backDrop}>
+      //     <div className="modal" style={modalStyle}>
+      //
+      //       <div className="footer">
+      //         <button onClick={this.props.onClose}>Close</button>
+      //       </div>
+      //     </div>
+      // </div>
+      <div className="backdropClass" style={backDrop}>
+        <Modal.Dialog style={modalStyle}>
+          <Modal.Header>
+            <Modal.Title>Login</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>Login in form will go in here</Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={this.props.onClose}>Close</Button>
+            <Button bsStyle="primary">Save changes</Button>
+          </Modal.Footer>
+        </Modal.Dialog>
       </div>
-    )
+    );
   }
+}
+
+Modal.PropTypes = {
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool,
+  children: PropTypes.node
 }
 
 export default ModalLoginComponent;
